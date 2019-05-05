94b71020122555511a4330aa77896f1adac1edec
ksaini <kunal_response@yahoo.co.in>
2019-05-05 19:58:48 +0530

    sbsj

--- www/js/dataTbl.js
+++ www/js/dataTbl.js
@@ -2,7 +2,7 @@ var dataArray=new Array();
 var schoolname= "JSS Public School";
 var address ="Sector 71, Delhi";
 var scn1 = "";
-var base_url = "http://greyboxerp.in/demoapp/";
+var base_url = "http://greyboxerp.in/sbsjapp/";
 
 
 function schoolDetails(data){
--- www/js/msg.js
+++ www/js/msg.js
@@ -1,5 +1,5 @@
 //var base_url = "http://localhost/pgexample/teacherapp/www/";
-var base_url = "http://greyboxerp.in/demoapp/";
+var base_url = "http://greyboxerp.in/sbsjapp/";
 var att = [];
 
 function getCID(){
